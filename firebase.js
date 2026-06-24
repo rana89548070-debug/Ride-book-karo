b3346269e878234625c91bb8a0f18474fa0cd0e3
rana89548070-debug <rana89548070@gmail.com>
2026-06-24 19:22:36 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBgmr-RNHwzrtvlELXi5OQCFco6hds6o2w",
   authDomain: "ride-book-karo-e83fd.firebaseapp.com",
   projectId: "ride-book-karo-e83fd",
-  storageBucket: "ride-book-karo-e83fd.firebasestorage.app",
+  storageBucket: "ride-book-karo-e83fd.appspot.com",
   messagingSenderId: "132089297625",
   appId: "1:132089297625:web:1cc6b4236b6918312c9cc8"
 };
